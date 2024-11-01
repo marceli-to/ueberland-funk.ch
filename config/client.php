a1e9a56e2c48e3cb7134a4a824bfcbf3d7ed45dd
Marcel Stadelmann <m@marceli.to>
2024-11-01 14:19:38 +0100

    wip

--- config/client.php
+++ config/client.php
@@ -1,5 +1,5 @@
 <?php
 return [
   'base_url' => env('APP_URL'),
-  'meta_description' => 'Wir machen Geschichten. Panda & Pinguin ist die Agentur für zeitgemässe Kommunikation.'
+  'meta_description' => 'Als zuverlässiger Partner rund um den 24-Stunden Telefon-Auftragsdienst sind wir seit 1978 für KMU‘s und nationale Unternehmen tätig. Unsere Stärke liegt in der Flexibilität, Zuverlässigkeit, Erfahrung und zwischendurch auch mal mit einer Prise Humor gemischt. Unser Motto: Qualität vor Quantität zu einem fairen Preis.'
 ];
